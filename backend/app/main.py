550689f4dadda1b454270fb82569d67427f90180
alex20k3 <alex20k3@gmail.com>
2025-12-13 09:42:03 +0300

    upd

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,33 +1,26 @@
 from fastapi import FastAPI
-from .db import Base, engine, SessionLocal
-from . import models
-from .routers import users, groups, expenses, karma
 from fastapi.middleware.cors import CORSMiddleware
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-FRONTEND_DIST = BASE_DIR / "frontend" / "dist"
+from .routers import auth, groups, expenses, karma
 
-app.mount("/", StaticFiles(directory=FRONTEND_DIST, html=True), name="frontend")
+app = FastAPI()
 
-app = FastAPI(title="Shared Expense Tracker")
-
+# 🚨 ВАЖНО: CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+    allow_origins=[
+        "https://expense-tracker-1-fsn3.onrender.com",  # фронт
+        "http://localhost:5173",                        # локалка
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# создаём таблицы
-Base.metadata.create_all(bind=engine)
-
-app.include_router(users.router, prefix="/api")
-app.include_router(groups.router, prefix="/api")
-app.include_router(expenses.router, prefix="/api")
-app.include_router(karma.router, prefix="/api")
-
-
+app.include_router(auth.router)
+app.include_router(groups.router)
+app.include_router(expenses.router)
+app.include_router(karma.router)
 
 
 @app.get("/")
